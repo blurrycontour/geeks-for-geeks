650c0b0d615f61c399d63d4fb75dc1a6f46c1a3e
Aditya Singh <adityasingh8532@gmail.com>
2023-08-22 10:46:21 +0000

    Solved D1

---
+++ potd/d1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def Count(self, matrix):
+        # Code here
+        rows = len(matrix)
+        columns = len(matrix[0])
+        _matrix = [[1]*(columns+2)]
+        for row in matrix:
+            _matrix.append([1]+row+[1])
+        _matrix.append([1]*(columns+2))
+        
+        ones = 0
+        for i in range(1,1+rows):
+            for j in range(1,1+columns):
+                if not _matrix[i][j]:
+                    continue
+                total = 0
+                total += _matrix[i-1][j-1]+_matrix[i-1][j]+_matrix[i-1][j+1]
+                total += _matrix[i][j-1]+_matrix[i][j+1]
+                total += _matrix[i+1][j-1]+_matrix[i+1][j]+_matrix[i+1][j+1]
+                total = 8-total
+                if (total % 2) == 0 and total > 0:
+                    ones += 1
+        return ones
+    
+if __name__ == "__main__":
+    s = Solution()
+    print(s.Count([[1,1,0],[0,1,0],[0,0,1]])) # 1
